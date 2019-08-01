f3c4d9bf07f63fd82a95f5400121c11c52bd0b50
Philalex <philalex196401@gmail.com>
2019-08-01 21:12:50 +0200

    1.0.0

---
+++ EQueoTest/EQueoTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EQueoTest
+//
+//  Created by Alexander Filippov on 8/1/19.
+//  Copyright © 2019 Alexander Filippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
